3cbe0b4ed9d9aaf6b5b4cccce847216378f8c4eb
YaroslavAver <155873788+Gravikko@users.noreply.github.com>
2026-03-09 09:00:04 +0300

    project id fix

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -3,6 +3,6 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 export const config = getDefaultConfig({
   appName: "NFT Marketplace",
-  projectId: "73d15791c604b548d31fa5e024399266", // ← замени на свой с cloud.walletconnect.com
+  projectId: "PROJECT_ID",
   chains: [sepolia],
 });
